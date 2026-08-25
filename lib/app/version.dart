f2647c3928b6930aa66840986ac97721de2787d0
Hillel Coren <hillelcoren@gmail.com>
2026-08-25 18:00:04 +0300

    Update version

--- lib/app/version.dart
+++ lib/app/version.dart
@@ -13,7 +13,7 @@ class AppVersion {
   AppVersion._();
 
   /// Sent as `X-CLIENT-VERSION` on every request.
-  static const String kClientVersion = '5.1.7';
+  static const String kClientVersion = '5.1.8';
 
   /// The minimum Invoice Ninja server version this client can talk to.
   ///
